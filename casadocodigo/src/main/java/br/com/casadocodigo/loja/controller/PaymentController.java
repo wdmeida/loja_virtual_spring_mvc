1321c9b3645bc19601e40c553b33047a77fbc6d1
Wagner Almeida <wdmeida@gmail.com>
2017-03-10 19:42:23 -0300

    Implementação do pagamento utilizando a api de testes hospedada no heroku.

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/controller/PaymentController.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/controller/PaymentController.java
@@ -1,6 +1,7 @@
 package br.com.casadocodigo.loja.controller;
 
 import java.math.BigDecimal;
+import java.util.concurrent.Callable;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -26,17 +27,30 @@ public class PaymentController {
 	@Autowired
 	private RestTemplate restTemplate;
 	
+	/*
+	 * Retorna um objeto do tipo Callable, que existe desde o Java 5 e é um análogo ao objeto tipo Runnable, muito
+	 * comum para quem usa Threads. A única diferença do Callable é que le permite dar um retorno, algo necessário
+	 * já que precisamos informar para qual endereço vamos depois da integração. Ao retornar um Callable, o Spring
+	 * MVC já vai criar e iniciar um contexto assíncrono em sua Servlet e liberar para que o Web Container (Tomcat)
+	 * possa usar a Thread dele para atender novas requisições. Assim, quando recebermos a resposta da integração 
+	 * com o módulo de pagamento, retornamos o endereço de redirect e o mesmo é informado ao Tomcat.
+	 * È importante ressaltar que não estamos tratando a performance do sistema, provavelmente o tempo de requisição
+	 * vai ser o mesmo. O que estamos tratando é a escalabilidade, tentanto manter o tempo médio de requisição mesmo
+	 * que a aplicação sofra com um acesso acima do normal.
+	 */
 	@RequestMapping(value = "checkout", method = RequestMethod.POST)
-	public String checkout() {
-		BigDecimal total = shoppingCart.getTotal();
-		
-		String uriToPay = "http://book-payment.herokuapp.com/payment";
-		
-		try {
-			String response = restTemplate.postForObject(uriToPay, new PaymentData(total), String.class);
-			return "redirect:/payment/success";
-		} catch (HttpClientErrorException e) {
-			return "redirect:/payment/error";
-		}
+	public Callable<String> checkout() {
+		return () -> {
+			BigDecimal total = shoppingCart.getTotal();
+			
+			String uriToPay = "http://book-payment.herokuapp.com/payment";
+			
+			try {
+				String response = restTemplate.postForObject(uriToPay, new PaymentData(total), String.class);
+				return "redirect:/payment/success";
+			} catch (HttpClientErrorException e) {
+				return "redirect:/payment/error";
+			}
+		};
 	}//checkout()
 }//class PaymentController
